c052d753076fbaa35cac72c191fc051b4231d9e2
Nas Villanueva <nasvillanuevaIT@gmail.com>
2015-04-19 10:52:22 +0800

    Fixed index.html and app.js for angular

--- src/main/webapp/resources/js/app.js
+++ src/main/webapp/resources/js/app.js
@@ -2,7 +2,7 @@
  * Created by NazIsEvil on 4/12/2015.
  */
 (function(){
-    var app = angular.module("PLVFES",["ngRoute","ngMaterial"]);
+    var app = angular.module("plvFes",["ngRoute","ngMaterial"]);
 
     app.controller('AppCtrl',function($scope){
 
